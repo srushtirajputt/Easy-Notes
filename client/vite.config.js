58623e8eb9d4e993373bb574c7ffaf9ca94a6c64
Shrushti <shrushti@example.com>
2026-04-29 01:43:58 +0530

    Allow ngrok hosts in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['.ngrok-free.app', '8b86-2409-40c1-2009-1995-9ddd-cd28-4378-8bd2.ngrok-free.app']
+  }
 })
